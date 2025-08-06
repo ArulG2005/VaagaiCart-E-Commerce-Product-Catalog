c80be9d5812f48e65da8f8f79f6751090a6b3b82
ArulG2005 <mailtogarul@gmail.com>
2025-08-06 16:23:50 +0530

    first commit

---
+++ backend/src/test/java/com/vaagai/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaagai.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
